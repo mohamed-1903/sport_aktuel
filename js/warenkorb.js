967e74e84f2cbfd71b86845c88c5e3cfa3eca832
Hussein-Alsumat <hua1999@thi.de>
2025-06-22 02:56:09 +0200

    commit

--- js/warenkorb.js
+++ js/warenkorb.js
@@ -62,6 +62,7 @@ function toggleCart(iid, btn = null, size = "M", qty = 1) {
           const price = parseFloat(btn.dataset.price) || 0;
           zeigeProduktPreview({ name, image, price, size, qty });
           updateCartCount();
+        }
         loadList();
       } else {
         zeigeToast("⚠️ Fehler: " + (data.error || "Unbekannt"), "#cc0000");
