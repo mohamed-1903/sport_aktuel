9e3ffed7e2c76eabdd3b75b2a9a92f5ddecc9cc8
mohamed-1903 <raoufmohamed0605@gmail.com>
2025-06-25 10:00:53 +0200

    Add cookie decline option and settings

--- view/layout/footer.php
+++ view/layout/footer.php
@@ -68,6 +68,15 @@
     <p>&copy; 2025 SportX. Alle Rechte vorbehalten.</p>
   </div>
 </footer>
+<div id="cookie-banner" class="cookie-banner">
+  <p>Diese Seite verwendet Cookies, um Funktionen wie den Warenkorb zu ermöglichen.</p>
+  <div class="cookie-buttons">
+    <button id="cookie-accept">Akzeptieren</button>
+    <button id="cookie-decline">Ablehnen</button>
+  </div>
+</div>
+<button id="cookie-settings" class="cookie-settings">Cookie-Einstellungen</button>
+<script src="js/cookies.js"></script>
 <script src="js/style_modification.js"></script>
 <script src="js/filterandsearch.js"></script>
 <script src="js/produkt.js"></script>
