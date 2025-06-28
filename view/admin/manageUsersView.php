5c80091af322e7e2a188878c0ec91a2c2607f66b
Hussein-Alsumat <hua1999@thi.de>
2025-06-28 20:47:57 +0200

    yasd

--- view/admin/manageUsersView.php
+++ view/admin/manageUsersView.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ . '/../layout/header.php'; ?>
 
-<section class="form-wrapper" style="padding: 2em; max-width: 1000px; margin-top: 15px;">
+<section class="form-wrapper" style="padding: 2em; max-width: 10000px; margin-top: 15px;">
   <h1 style="text-align: center;">👥 Benutzerverwaltung</h1>
   <?php if (empty($allUsers)): ?>
     <p style="text-align: center;">Es wurden keine Benutzer gefunden.</p>
@@ -8,7 +8,7 @@
     <table class="cart-table">
       <thead>
         <tr>
-          <th>#</th>
+          <th>Id-#</th>
           <th>Benutzername</th>
           <th>E-Mail</th>
           <th>Registriert am</th>
@@ -24,11 +24,11 @@
             <td><?= htmlspecialchars($user['email']) ?></td>
             <td><?= date("d.m.Y H:i", strtotime($user['created_at'])) ?></td>
             <td><?= htmlspecialchars($user['status']) ?></td>
-            <td style="display:flex;gap:0.5em;">
+            <td style="display: flex; gap: 0.5em; justify-content: center; align-items: center;">
               <!-- Inaktiv: <a href="#"><button>Löschen</button></a> -->
               <form action="index.php?page=admin&action=deleteUser" method="post" onsubmit="return confirm('Benutzer wirklich löschen?');">
                 <input type="hidden" name="user_id" value="<?= (int)$user['id'] ?>">
-                <button type="submit" class="btn-delete-all">❌ Entfernen</button>
+                <button type="submit" class="btn-delete-all">Entfernen</button>
               </form>
               <form action="index.php?page=admin&action=toggleUserStatus" method="post">
                 <input type="hidden" name="user_id" value="<?= (int)$user['id'] ?>">
