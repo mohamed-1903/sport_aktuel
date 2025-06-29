6824db16e372f815d3f23f0fa5a1eeafbed46489
mohamed-1903 <raoufmohamed0605@gmail.com>
2025-06-29 15:07:43 +0200

    Show three related products

--- view/product/productDetailView.php
+++ view/product/productDetailView.php
@@ -177,7 +177,8 @@
       </div>
 
     </section>
-  <?php endforeach; ?>
+    <?php endforeach; ?>
+
 
   <button id="showCompareBtn" class="compare-toggle-btn" aria-label="Vergleich öffnen" type="button">+</button>
   <div id="compareSection" class="compare-section hidden">
@@ -197,6 +198,57 @@
     </datalist>
     <button id="compareBtn" class="btn-compare">⚖️ Vergleichen</button>
   </div>
+
+  <?php if (!empty($similarProducts)): ?>
+    <section class="similar-products">
+      <h3>Ähnliche Produkte</h3>
+      <ul class="einzelprodukt-grid">
+        <?php foreach ($similarProducts as $sp): ?>
+          <?php
+            $preis = (float)($sp['priceValue'] ?? $sp['price']);
+            $discount = $sp['discount'] ?? 0;
+            $salePrice = $discount > 0 ? $preis * (1 - $discount / 100) : $preis;
+            $marke = $sp['marke'] ?? '';
+            $farbe = $sp['farbe'] ?? '';
+            $geschlecht = $sp['geschlecht'] ?? '';
+            $mannschaft = stripos($sp['name'] ?? '', 'Bayern') !== false ? 'Bayern' : (stripos($sp['name'] ?? '', 'Dortmund') !== false ? 'Dortmund' : '');
+          ?>
+          <li class="einzelprodukt"
+            data-id="<?= (int)($sp['id'] ?? 0) ?>"
+            data-marke="<?= htmlspecialchars($marke) ?>"
+            data-farbe="<?= htmlspecialchars($farbe) ?>"
+            data-preis="<?= $preis ?>"
+            data-mannschaft="<?= $mannschaft ?>"
+            data-geschlecht="<?= htmlspecialchars($geschlecht) ?>">
+            <div class="image-wrapper">
+              <img src="<?= htmlspecialchars($sp['image_main'] ?? '') ?>" alt="<?= htmlspecialchars($sp['name'] ?? '') ?>">
+            </div>
+            <div class="produkt-info">
+              <h3><?= htmlspecialchars($sp['name'] ?? '') ?></h3>
+              <?php if ($discount > 0): ?>
+                <p>
+                  <del class="old-price">
+                    <?= number_format($preis, 2, ',', '.') ?>€ inkl. Mwst.
+                  </del>
+                  <span><?= number_format($salePrice, 2, ',', '.') ?>€ inkl. Mwst.</span>
+                  <span class="rabatt">-<?= $discount ?>%</span>
+                </p>
+              <?php else: ?>
+                <p><?= number_format($preis, 2, ',', '.') ?>€ <span>inkl. Mwst.</span></p>
+              <?php endif; ?>
+            </div>
+            <div class="button-row" data-iid="<?= (int)$sp['id'] ?>">
+              <a href="index.php?page=product&action=detail&id=<?= (int)$sp['id'] ?>">
+                <button>Details</button>
+              </a>
+              <button class="btn-add-to-cart" data-iid="<?= (int)$sp['id'] ?>" data-name="<?= htmlspecialchars($sp['name'] ?? '') ?>" data-price="<?= $salePrice ?>" data-discount="<?= $discount ?>" data-image="<?= htmlspecialchars($sp['image_main'] ?? '') ?>">🛒</button>
+              <button class="btn-add-to-watch" data-iid="<?= (int)$sp['id'] ?>" data-name="<?= htmlspecialchars($sp['name'] ?? '') ?>" data-price="<?= $salePrice ?>" data-discount="<?= $discount ?>" data-image="<?= htmlspecialchars($sp['image_main'] ?? '') ?>">🤍</button>
+            </div>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </section>
+  <?php endif; ?>
 
 
   <?php foreach ($productsToShow as $index => $product):
